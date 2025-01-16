944fbb83622d2420a18a7cd9bfdb47ada5e31a37
Sotiris Morfakidis <sotirismorfakidis@gmail.com>
2025-01-16 18:58:40 +0200

    add check script

---
+++ check.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+diff --color -u ./alacritty.toml ~/.config/alacritty/alacritty.toml
+diff --color -u ./tmux.conf      ~/.tmux.conf
+diff --color -u ./swayconfig     ~/.config/sway/config
