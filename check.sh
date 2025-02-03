caa8709f1f128172849b2f9282d0e05fd55e6e73
Sotiris Morfakidis <sotirismorfakidis@gmail.com>
2025-02-03 15:44:58 +0200

    add waybar config

--- check.sh
+++ check.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-diff --color -u ./alacritty.toml ~/.config/alacritty/alacritty.toml
-diff --color -u ./tmux.conf      ~/.tmux.conf
-diff --color -u ./swayconfig     ~/.config/sway/config
+diff --color -u    ./alacritty.toml ~/.config/alacritty/alacritty.toml
+diff --color -u    ./swayconfig     ~/.config/sway/config
+diff --color -u    ./tmux.conf      ~/.tmux.conf
+diff --color -u -r ./waybar       ~/.config/waybar
